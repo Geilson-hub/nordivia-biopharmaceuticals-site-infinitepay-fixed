8eb1d6eec8be79c5cc371647ea88be1a5be1fb75
Geilson-hub <gs.mendesa@gmail.com>
2026-08-01 11:02:45 -0300

    fix: revert readFileSync to dynamic import for messages (now __dirname is fixed by Vercel preset)

--- i18n.ts
+++ i18n.ts
@@ -1,6 +1,4 @@
 import { getRequestConfig } from "next-intl/server";
-import { readFileSync } from "fs";
-import { join } from "path";
 import { locales, defaultLocale } from "./i18n/locales";
 
 export default getRequestConfig(async ({ locale }) => {
@@ -9,11 +7,8 @@ export default getRequestConfig(async ({ locale }) => {
       ? locale
       : defaultLocale;
 
-  const filePath = join(process.cwd(), "messages", `${currentLocale}.json`);
-  const messages = JSON.parse(readFileSync(filePath, "utf-8"));
-
   return {
     locale: currentLocale,
-    messages,
+    messages: (await import(`./messages/${currentLocale}.json`)).default
   };
 });
