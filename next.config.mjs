b0f4ca08d6dec74af8128329a15ffd6ba4c657fb
Geilson-hub <gs.mendesa@gmail.com>
2026-08-01 10:11:15 -0300

    fix: externalize Prisma from webpack to fix __dirname error on Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,13 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ["@prisma/client"],
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals || [];
+      config.externals.push("@prisma/client");
+    }
+    return config;
+  },
 };
 
 export default withNextIntl(nextConfig);
