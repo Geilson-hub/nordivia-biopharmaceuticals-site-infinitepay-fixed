c42351231c88a5c0b956001171f4290a637e388a
Geilson-hub <gs.mendesa@gmail.com>
2026-08-01 00:22:13 -0300

    Configure GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: '/nordivia-biopharmaceuticals-site-infinitepay-fixed',
+  output: 'export',
+};
 
 export default withNextIntl(nextConfig);
